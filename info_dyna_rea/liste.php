efadfc20c1c5dc7edc649523c4d646ef58dc4c8c
Fukalou <thomas.duprez10@gmail.com>
2017-12-13 15:20:50 +0100

    Fin de la dynamisation de realisateur.php

--- info_dyna_rea/liste.php
+++ info_dyna_rea/liste.php
@@ -1,17 +1,22 @@
+<section class="Image_du_film">
+	<h3>Acteur Fétiches :</h3>
+	<ul>
 <?php
 	include 'connect.php';
 	
-	$reponse = $bdd->query('SELECT * FROM picture WHERE id=1');
-	$donnee = $reponse->fetch();
-
-	$reponse2 = $bdd->query('SELECT * FROM picture WHERE id=2');
-	$donnee2 = $reponse2->fetch();
-?>
-
-<section>
-	<h3>Acteur Fétiches :</h3>
-	<ul>
-		<li><img alt="<?php echo $donnee['legend']; ?>" src="<?php echo $donnee['path']; ?>"/></li>
-		<li><img alt="<?php echo $donnee2['legend']; ?>" src="<?php echo $donnee2['path']; ?>"/></li>
+	$id = $_GET['id'];
+	$reponse = $bdd->query('SELECT idActeur FROM acteurFeticheHasRealisateur WHERE idRealisateur="'.$id.'"');
+	while($donnee = $reponse->fetch())
+	{
+		$reponse2 = $bdd->query('SELECT idPicture FROM personHasPicture WHERE idPerson="'.$donnee['idActeur'].'"');
+		while($donnee_num_picture = $reponse2->fetch())
+		{
+			$reponse3 = $bdd->query('SELECT * FROM picture WHERE id="'.$donnee_num_picture['idPicture'].'"');
+			while($donnee_picture = $reponse3->fetch())	
+			{ ?>
+						<li><img alt="<?php echo $donnee_picture['legend']; ?>" src="<?php echo $donnee_picture['path']; ?>"/></li>
+	<?php 	}
+		}
+	}?>
 	</ul>
 </section>
